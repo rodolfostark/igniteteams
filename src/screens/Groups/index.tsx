e3d44e935c55754c049d8c069ce347054ee707ee
rodolfostark <rodolfojonatas@outlook.com>
2025-04-16 21:23:54 -0300

    :sparkles: feat: setup and configure styled-components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Container, Title } from './styles';
+
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
+    <Container>
+      <Title>
+        Groups
+      </Title>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
