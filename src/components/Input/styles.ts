51c4077430b999bb0e97d452b4515054af1ed6a7
rodolfostark <rodolfojonatas@outlook.com>
2025-04-21 18:47:37 -0300

    :recycle: refactor: add css helper to all left styles

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,14 +1,16 @@
 import { TextInput } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(TextInput)`
     flex: 1;
     min-width: 56px;
     max-width: 56px;
-    background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-    color: ${({ theme }) => theme.COLORS.WHITE};
-    font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+    ${({ theme }) => css`
+        background-color: ${theme.COLORS.GRAY_700};
+        color: ${theme.COLORS.WHITE};
+        font-family: ${theme.FONT_FAMILY.REGULAR};
+        font-size: ${theme.FONT_SIZE.MD}px;
+    `};
     border-radius: 6px;
     padding: 16px;
 `;
--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -4,7 +4,6 @@ export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-
     background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 `;
 
