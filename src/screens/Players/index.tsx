decbf60b6a5be6243e3ae122a7ce50781abe2794
rodolfostark <rodolfojonatas@outlook.com>
2025-04-22 19:48:04 -0300

    :sparkles: create the button icon component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,9 @@
 import { Header } from "@/components/Header";
-import { Container } from "./styles";
 import { Highlight } from "@/components/Highlight";
+import { ButtonIcon } from "@/components/ButtonIcon";
+import { Input } from "@/components/Input";
+
+import { Container, Form } from "./styles";
 
 export function Players() {
     return (
@@ -10,6 +13,16 @@ export function Players() {
                 title="Nome da turma"
                 subtitle="adicione a galera e separe os times"
             />
+            <Form>
+                <Input 
+                    placeholder="Nome da pessoa"
+                    autoCorrect={false}
+                />
+                <ButtonIcon 
+                    icon="add"
+                    type="PRIMARY" 
+                />
+            </Form>
         </Container>
     );
 }
