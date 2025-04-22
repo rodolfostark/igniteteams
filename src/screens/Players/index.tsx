a30a157aeba98b0141057f7ddd6fe3d0f8e3b6d0
rodolfostark <rodolfojonatas@outlook.com>
2025-04-22 20:41:00 -0300

    :sparkles: create the players list component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Filter } from "@/components/Filter";
 import { Header } from "@/components/Header";
 import { Highlight } from "@/components/Highlight";
 import { ButtonIcon } from "@/components/ButtonIcon";
 import { Input } from "@/components/Input";
 
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+    const [team, setTeam] = useState('Time A');
+    const [players, setPLayers] = useState([])
+
     return (
         <Container>
             <Header showBackButton />
@@ -24,9 +30,23 @@ export function Players() {
                     type="PRIMARY" 
                 />
             </Form>
-            <Filter 
-                title="Time A"
-            />
+            <HeaderList>
+                <FlatList 
+                    data={['Time A', 'Time B']}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter 
+                            title={item} 
+                            isActive={item === team} 
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+                <NumberOfPlayers>
+                    { players.length }
+                </NumberOfPlayers>
+            </HeaderList>
         </Container>
     );
 }
